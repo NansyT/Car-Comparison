2dc9d369046a3015b4da249a5bc2d21f5b74d8bd
NansyT <nanna.tronbjerg@gmail.com>
2022-08-31 15:59:38 +0200

    Did some backend stuff

---
+++ CarComparisonSite/CarComparisonSite/Fuel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarComparisonSite
+{
+    public class Fuel
+    {
+        private FuelType fuelType;
+
+        public FuelType FuelType
+        {
+            get { return fuelType; }
+            private set { fuelType = value; }
+        }
+
+
+        private float pricePerUnit;
+
+        public float PricePerUnit
+        {
+            get { return pricePerUnit; }
+            private set { pricePerUnit = value; }
+        }
+
+        public Fuel(FuelType fuelType, float pricePerUnit)
+        {
+            FuelType = fuelType;
+            PricePerUnit = pricePerUnit;
+        }
+    }
+}
