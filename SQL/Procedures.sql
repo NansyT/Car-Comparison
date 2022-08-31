874aa573e923251e12c8bf545ee6638942675ef4
NansyT <nanna.tronbjerg@gmail.com>
2022-08-31 13:05:01 +0200

    Create Procedures.sql

---
+++ SQL/Procedures.sql
@@ -0,0 +1,26 @@
+CREATE OR ALTER PROCEDURE GetFilters
+AS
+	SELECT [Brand].[BName], [Model].[MName] FROM [Brand], [Model];
+
+GO
+
+CREATE OR ALTER PROCEDURE GetCars @brand VARCHAR(30), @model VARCHAR(50), @variant VARCHAR(150), @releaseYear DATE
+AS
+	SELECT * FROM [Car]
+	WHERE
+	BName LIKE @brand
+	AND
+	MName LIKE @model
+	AND
+	Variant LIKE @variant
+	AND
+	ReleaseYear LIKE @releaseYear
+
+	SELECT * FROM [Car]
+	WHERE
+	BName LIKE '%'+@brand+'%'
+	AND
+	MName LIKE '%'+@model+'%'
+	AND
+	Variant LIKE '%'+@variant+'%'
+GO
