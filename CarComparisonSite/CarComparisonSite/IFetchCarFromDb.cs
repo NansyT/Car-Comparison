d921f13c3d8f768d1d023e8ba249035cf218eff5
NansyT <nanna.tronbjerg@gmail.com>
2022-09-01 11:34:38 +0200

    Backend stuff
    created interface and implemented it

---
+++ CarComparisonSite/CarComparisonSite/IFetchCarFromDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarComparisonSite
+{
+    interface IFetchCarFromDb
+    {
+        public List<Car> GetCars(string variant, string model, DateTime releaseYear, Brand? brand);
+
+        public FilterOptions GetOnLoad();
+    }
+}
--- CarComparisonSite/CarComparisonSite/MsSqlConnection.cs
+++ CarComparisonSite/CarComparisonSite/MsSqlConnection.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace CarComparisonSite
 {
-    public class MsSqlConnection
+    public class MsSqlConnection : IFetchCarFromDb
     {
-        
+        private SqlConnection connection;
+        private SqlCommand command;
+        private IConfiguration config;
+
+        public MsSqlConnection(IConfiguration configuration)
+        {
+            this.config = configuration;
+        }
+
+        public List<Car> GetCars(string variant, string model, DateTime releaseYear, Brand? brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FilterOptions GetOnLoad()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
