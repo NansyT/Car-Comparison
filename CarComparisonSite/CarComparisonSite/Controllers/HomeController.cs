d6ef8ad31f97a7e3f0cc80e02c3814694644ec37
Kenneth Folke Larsen <kenn5073@zbc.dk>
2022-10-11 08:28:38 +0200

    Fixed stuff

--- CarComparisonSite/CarComparisonSite/Controllers/HomeController.cs
+++ CarComparisonSite/CarComparisonSite/Controllers/HomeController.cs
@@ -28,15 +28,195 @@ namespace CarComparisonSite.Controllers
             cars = dbConnector.GetAllCars();
             pageNumber = page ?? 1;
             cars = FilterCars(cars);
-
+            //set up page view with X.pagedlist
             onePageOfCars = cars.ToPagedList(pageNumber, 10);
             ViewBag.OnePageOfCars = onePageOfCars;
+            //collect the items for the first dropdown
             GetAllBrands();
-            //FilterCars(modelFilter, variantFilter, dateFilter, brandfilter);
             return View();
         }
 
+        //Called from an onclick in JS
+        //Sends us the carId that was clicked,
+        //so we can save it in session and be used in the view for selected cars
+        //Viewbag and TempData does not last long, so session is used
+        [HttpPost]
+        public ActionResult Index(int carId)
+        {
+            cars = dbConnector.GetAllCars();
+            for (int i = 0; i < cars.Count; i++)
+            {
+                if (cars[i].CarId == carId)
+                {
+                    if (cars[i].Fuel.FuelType == FuelType.Benzin)
+                    {
+                        HttpContext.Session.SetObject("SelectedGas", cars[i]);
+                    }
+                    else
+                    {
+                        HttpContext.Session.SetObject("SelectedElectric", cars[i]);
+                    }
+                    i = cars.Count + 1;
+                }
+            }
+            return RedirectToAction("Index");
+        }
 
+        [HttpGet]
+        public ActionResult GetAllBrands()
+        {
+            try
+            {
+                List<Brand> brands = ((MsSqlConnection)dbConnector).GetAllBrands();
+                if (brands != null && brands.Count > 0)
+                {
+                    HttpContext.Session.SetObject("brands", brands);
+                    return Ok();
+                }
+                else
+                {
+                    throw new Exception("The returned list was null or no elements were found");
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult SelectBrand(Brand brand)
+        {
+            try
+            {
+                if (HttpContext.Session.GetObject<Brand>("currentBrand") != brand)
+                {
+                    HttpContext.Session.SetObject("currentBrand", brand);
+                    ResetButtons();
+                    SetAVailableModels(brand);
+                }
+                return RedirectToAction("index");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets all models that has the selected <paramref name="brand"/> and sets them in session
+        /// </summary>
+        /// <param name="brand"></param>
+        /// <exception cref="Exception"></exception>
+        private void SetAVailableModels(Brand brand)
+        {
+            List<string> models = ((MsSqlConnection)dbConnector).GetModelsByBrand(brand);
+            if (models != null && models.Count > 0)
+            {
+                HttpContext.Session.SetObject("models", models);
+            }
+            else
+            {
+                throw new Exception("The returned list was null or no elements were found");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult SelectModel(string model)
+        {
+            try
+            {
+                if (HttpContext.Session.GetObject<string>("currentModel") != model)
+
+                {
+                    HttpContext.Session.SetObject("currentModel", model);
+                    SetAvailableVariants(model);
+                }
+                return RedirectToAction("index");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        private void SetAvailableVariants(string model)
+        {
+            List<string> variants = ((MsSqlConnection)dbConnector).GetVariantsByModel(model);
+            if (variants != null && variants.Count > 0)
+            {
+                HttpContext.Session.SetObject("variants", variants);
+            }
+            else
+            {
+                throw new Exception("The returned list was null or no elements were found");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult SelectVariant(string variant = "")
+        {
+            try
+            {
+                HttpContext.Session.SetObject("currentVariant", variant);
+
+                Brand? brand = HttpContext.Session.GetObject<Brand?>("currentBrand");
+                string model = HttpContext.Session.GetObject<string>("currentModel");
+
+                List<int> years = ((MsSqlConnection)dbConnector).GetYears(brand, variant, model);
+                if (years != null && years.Count > 0)
+                {
+                    HttpContext.Session.SetObject("years", years);
+
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    return StatusCode(500, "error");
+                }
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        private void SetAvailableYears(string variant)
+		{
+
+		}
+
+        [HttpPost]
+        public ActionResult SelectYear(int year)
+        {
+            HttpContext.Session.SetObject("currentYear", year);
+            //Brand brand = HttpContext.Session.GetObject<Brand>("currentBrand");
+            //SelectBrand(brand);
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public ActionResult SetKmPerYear(int kmYear)
+        {
+            if (kmYear != 0)
+            {
+                HttpContext.Session.SetObject("kmYear", kmYear);
+            }
+            return RedirectToAction("index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// Tries to get every current filter and remove elements from <paramref name="cars"/> that doesn't fit those criteria
+        /// </summary>
+        /// <param name="cars">The list to filter on</param>
+        /// <returns>returns a filtered list of cars</returns>
         private List<Car> FilterCars(List<Car> cars)
         {
             Brand? brand = HttpContext.Session.GetObject<Brand?>("currentBrand");
@@ -46,6 +226,7 @@ namespace CarComparisonSite.Controllers
 
             if (brand != null)
             {
+                //Loop backwards through the elements, as to not skip any or go out of bounds
                 for (int i = cars.Count - 1; i > -1; i--)
                 {
                     if (cars[i].Brand != brand)
@@ -87,69 +268,6 @@ namespace CarComparisonSite.Controllers
 
             return cars;
         }
-
-        [HttpGet]
-        public ActionResult ClearFilters()
-        {
-
-            HttpContext.Session.SetObject("model", null);
-            HttpContext.Session.SetObject("variant", null);
-            HttpContext.Session.SetObject("brand", null);
-            HttpContext.Session.SetObject("year", 0);
-            return RedirectToAction("index");
-        }
-
-        [HttpGet]
-        public ActionResult GetAllBrands()
-        {
-            try
-            {
-                List<Brand> brands = ((MsSqlConnection)dbConnector).GetAllBrands();
-                if (brands != null && brands.Count > 0)
-                {
-                    HttpContext.Session.SetObject("brands", brands);
-                    return Ok();
-                }
-                else
-                {
-                    throw new Exception("The returned list was null or no elements were found");
-                }
-            }
-            catch (Exception e)
-            {
-
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult SelectBrand(Brand brand)
-        {
-            try
-            {
-                if (HttpContext.Session.GetObject<Brand>("currentBrand") != brand)
-                {
-                    HttpContext.Session.SetObject("currentBrand", brand);
-                    ResetButtons();
-                }
-
-                List<string> models = ((MsSqlConnection)dbConnector).GetModelsByBrand(brand);
-                if (models != null && models.Count > 0)
-                {
-                    HttpContext.Session.SetObject("models", models);
-                    return RedirectToAction("index");
-                }
-                else
-                {
-                    throw new Exception("The returned list was null or no elements were found");
-                }
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, e.Message);
-            }
-        }
-
         private void ResetButtons()
         {
             HttpContext.Session.SetObject("currentModel", null);
@@ -158,109 +276,6 @@ namespace CarComparisonSite.Controllers
             HttpContext.Session.SetObject("variants", null);
             HttpContext.Session.SetObject("currentYear", null);
             HttpContext.Session.SetObject("years", null);
-        }
-
-        [HttpPost]
-        public ActionResult SelectModel(string model)
-        {
-            try
-            {
-                HttpContext.Session.SetObject("currentModel", model);
-
-                List<string> variants = ((MsSqlConnection)dbConnector).GetVariantsByModel(model);
-                if (variants != null && variants.Count > 0)
-                {
-                    HttpContext.Session.SetObject("variants", variants);
-                    return RedirectToAction("index");
-                }
-                else
-                {
-                    throw new Exception("The returned list was null or no elements were found");
-                }
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult SelectVariant(string variant = "")
-        {
-            HttpContext.Session.SetObject("currentVariant", variant);
-
-            Brand brand = HttpContext.Session.GetObject<Brand>("currentBrand");
-            string model = HttpContext.Session.GetObject<string>("currentModel");
-
-            List<int> years = ((MsSqlConnection)dbConnector).GetYears(brand, variant, model);
-            if (years != null && years.Count > 0)
-            {
-                HttpContext.Session.SetObject("years", years);
-
-                return RedirectToAction("index");
-            }
-            else
-            {
-                return StatusCode(500, "error");
-            }
-        }
-
-        [HttpPost]
-        public ActionResult SelectYear(int year)
-        {
-            HttpContext.Session.SetObject("currentYear", year);
-            Brand brand = HttpContext.Session.GetObject<Brand>("currentBrand");
-            SelectBrand(brand);
-            return RedirectToAction("index");
-        }
-
-        [HttpPost]
-        public ActionResult SetKmPerYear(int kmYear)
-        {
-            if (kmYear != 0)
-            {
-                HttpContext.Session.SetObject("kmYear", kmYear);
-            }
-            return RedirectToAction("index");
-        }
-
-        //Called from an onclick in JS
-        //Sends us the carId that was clicked,
-        //so we can save it in session and be used in the view for selected cars
-
-        //Viewbag and TempData does not last long, so session is required
-        [HttpPost]
-        public ActionResult Index(int carId, double kmYear = 0)
-        {
-            cars = dbConnector.GetAllCars();
-            for (int i = 0; i < cars.Count; i++)
-            {
-                if (cars[i].CarId == carId)
-                {
-                    if (cars[i].Fuel.FuelType == FuelType.Benzin)
-                    {
-                        HttpContext.Session.SetObject("SelectedGas", cars[i]);
-                    }
-                    else
-                    {
-                        HttpContext.Session.SetObject("SelectedElectric", cars[i]);
-                    }
-                    i = cars.Count + 1;
-                }
-            }
-            //if (kmYear != 0)
-            //{
-            //    HttpContext.Session.SetObject("kmYear", kmYear);
-
-            //}
-            return RedirectToAction("Index");
-        }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
